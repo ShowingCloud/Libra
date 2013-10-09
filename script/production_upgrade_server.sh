3392d6c4c3c9cff8fa18de3195e88a887a9d6e44
WANG Guoqin <wangguoqin1001@gmail.com>
2013-10-09 16:48:29 +0800

    Fixed misuse of variable

--- script/production_upgrade_server.sh
+++ script/production_upgrade_server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -x
+
 RAILS_ENV=production
+export RAILS_ENV
 
 git pull
 rake db:migrate
-rake db:seed
 #rake db:data:load_dir dir="datadump"
 #rm -rf vendor/assets/images
 #cp -r ../testing/vendor/assets/images/ vendor/assets/images/
