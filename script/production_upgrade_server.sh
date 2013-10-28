ee9fdbb0e69d3217e2154b6db7fcf68c55478dbd
WANG Guoqin <wangguoqin1001@gmail.com>
2013-10-28 16:54:17 +0800

    Refresh thumbnails on upgrading

--- script/production_upgrade_server.sh
+++ script/production_upgrade_server.sh
@@ -7,8 +7,12 @@ export RAILS_ENV
 
 git pull
 rake db:migrate
+
 #rake db:data:load_dir dir="datadump"
 #rm -rf vendor/assets/images
 #cp -r ../testing/vendor/assets/images/ vendor/assets/images/
+
+rake paperclip:refresh:thumbnails CLASS=Spree::Image
+
 rake assets:clean
 rake assets:precompile
