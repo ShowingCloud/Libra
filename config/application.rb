8998db4093b2ee0edfcc309b80cd8bbdce1ca9fe
WANG Guoqin <wangguoqin1001@gmail.com>
2013-10-28 16:35:28 +0800

    Revert "mod size product photo"
    This reverts commit 5be298e4e752625ea286fdc51e07781a3cd30304.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module Libra
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = "en"
+    config.i18n.default_locale = "zh-CN"
   end
 end
