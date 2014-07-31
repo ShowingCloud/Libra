5fd6e8204149e3297c769e09a7525c64fac6d3c1
WANG Guoqin <wangguoqin1001@gmail.com>
2014-07-31 01:40:14 -0400

    new production port no

--- script/production_start_server.sh
+++ script/production_start_server.sh
@@ -4,4 +4,4 @@ set -x
 
 [ -f tmp/pids/server.pid ] && kill $(cat tmp/pids/server.pid)
 
-rails s -d -p 3210 -e production -P `pwd`/tmp/pids/server.pid
+rails s -d -p 3220 -e production -P `pwd`/tmp/pids/server.pid
