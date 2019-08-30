38d102e1cae1c00ccf3de7bc095c0c73989de213
www-data @ SCS.IM <www-data@scs.im>
2019-08-30 14:26:16 -0400

    Forcibly loading Arel.rb to fix the problem of visit_Integer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+require_relative 'initializers/arel'
+
 # Initialize the Rails application.
 Libra::Application.initialize!
